fa07c32cbdb9ba9bed62635c4dcf0291ad807325
mikeydebug <cs24b1031@iiitr.ac.in>
2025-12-27 23:46:26 +0530

    Add booking and favourites pages with detailed layouts and functionality
    - Implemented bookings.ejs to display user's bookings with details and actions.
    - Created favourate-list.ejs for users to view their saved homes.
    - Developed home-detail.ejs to show detailed information about a selected home.
    - Added home-list.ejs to list available homes with options to book.
    - Designed index.ejs as the landing page with search functionality and featured sections.
    - Created reserve.ejs to confirm reservations with booking details and actions.

--- controller/home.js
+++ controller/home.js
@@ -1,28 +1,43 @@
-
+const Home = require("../models/home_model.js");
+//import { registeredHomes } from "../model/home"; // Import the registeredHomes array
 //controller/home.js
 //get add home
 exports.getAddHome = (req, res,next) => {
-    res.render("add-home",{title: 'Add Home - StayNest', activePage: 'add-home'});       
+    res.render("host/add-home",{title: 'Add Home - StayNest', activePage: 'add-home'});       
 }
 
 //post add home
-const registeredHomes = [];
+
 exports.postAddHome = (req, res,next) => {
-    console.log('Home Details:', req.body);
-    registeredHomes.push({title: req.body.title, description: req.body.description, price: req.body.price , location: req.body.location, imageUrl: req.body.imageUrl, rating: req.body.rating || 4.5});
-    res.render("homeadded",{title: 'Home Added - StayNest', activePage: ''});
+
+
+    // const newhome = new Home(req.body.title, req.body.description, req.body.price, req.body.location, req.body.imageUrl, req.body.rating );
+    // newhome.save();    
+    //.       OR 
+
+    const { title, description, price, location, imageUrl, rating } = req.body;// destructuring form body 
+    const newhome = new Home(title, description, price, location, imageUrl, rating);
+    newhome.save();
+
+    // Alternatively, you can push directly to registeredHomes if needed
+    //
+
+
+
+    res.render("host/homeadded",{title: 'Home Added - StayNest', activePage: ''});
 }
 
 //no need to export registeredHomes from here becz we can access it directly in homeController
 //exports.registeredHomes = registeredHomes;
 
 
+
 // get home details
 exports.getHomeDetails  = (req, res, next) => {
-
-    console.log('Registered Homes:',registeredHomes);
-    // Render the home view and pass the registeredHomes array to it
-    res.render("home",{registeredHomes: registeredHomes, title: 'Home - StayNest', activePage: 'home'});
-    
+    const registeredHomes = Home.fetchAll(registeredHomes => {
+        res.render("store/home-list",{registeredHomes: registeredHomes, 
+            title: 'Home - StayNest', 
+            activePage: 'home'});
+    });    
 
 }
--- models/home_model.js
+++ models/home_model.js
@@ -1,3 +1,52 @@
+
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/pathutil');
+
+
+
+
+
+
+
+const registeredHomes = [];
+
+
 module.exports = class Home {
-    constructor(id, title, description, price, location, hostId) {
-        this.id = id;
+    constructor( title, description, price, location, imageUrl, rating) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.location = location;
+        this.imageUrl = imageUrl;
+        this.rating = rating;
+    }
+
+    save() {
+        Home.fetchAll(registeredHomes => {
+        registeredHomes.push(this);
+        const homeDataPath = path.join(rootDir, 'data', 'homes_data.json');
+
+        fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (err) => {
+            if (err) {
+                console.log('Error writing file', err);
+            }
+
+        });
+    });
+
+    }
+
+    static fetchAll(callback) {
+        const homeDataPath = path.join(rootDir, 'data', 'homes_data.json');
+        fs.readFile(homeDataPath, (err, data) => {
+            if (!err) {
+                callback(JSON.parse(data));
+            }
+            else {
+            callback([]);
+            }
+        });
+
+    }
+}
