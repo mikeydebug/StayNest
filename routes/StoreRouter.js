6eaab19875648300b27adda82212624988381c94
mikeydebug <cs24b1031@iiitr.ac.in>
2026-07-12 11:08:31 +0530

    Add isAuth middleware to protect store routes like favorites and bookings

--- routes/StoreRouter.js
+++ routes/StoreRouter.js
@@ -8,16 +8,16 @@ const express = require("express");
 const StoreRouter = express.Router();
 
 const StoreController = require("../controller/StoreController");
-
+const isAuth = require("../middleware/isAuth");
 
 StoreRouter.get("/", StoreController.getIndex);
 StoreRouter.get("/homes", StoreController.getHomeDetails);
-StoreRouter.get("/favorites", StoreController.getfavorites);
-StoreRouter.post("/favorites", StoreController.postAddfavorites);
-StoreRouter.post("/favorites/delete/:homeId", StoreController.postDeleteFavorite);
-StoreRouter.get("/bookings", StoreController.getbookings);
-StoreRouter.post("/bookings/create", StoreController.postCreateBooking);
-StoreRouter.post("/bookings/cancel/:bookingId", StoreController.postCancelBooking);
+StoreRouter.get("/favorites", isAuth, StoreController.getfavorites);
+StoreRouter.post("/favorites", isAuth, StoreController.postAddfavorites);
+StoreRouter.post("/favorites/delete/:homeId", isAuth, StoreController.postDeleteFavorite);
+StoreRouter.get("/bookings", isAuth, StoreController.getbookings);
+StoreRouter.post("/bookings/create", isAuth, StoreController.postCreateBooking);
+StoreRouter.post("/bookings/cancel/:bookingId", isAuth, StoreController.postCancelBooking);
 //view home details
 StoreRouter.get("/homes/:id", StoreController.getViewDetails);
 
